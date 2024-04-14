2c5b8495fef06f0c2a79cc2209ab35c63d20ff6a
nicollaswendel <nicollaswendel1623@gmail.com>
2024-04-13 22:10:45 -0300

    Message

---
+++ js/salvarCep.js
@@ -0,0 +1,56 @@
+const botaoSalvar = document.getElementById('salvar')
+const botaoLimpar = document.getElementById('limpar')
+
+const setDadosForm = function (dados){
+    let rua = document.getElementById('logradouro')
+    rua.value = dados.logradouro
+
+    let bairro = document.getElementById('bairro')
+    bairro.value = dados.bairro
+
+    let cidade = document.getElementById('cidade')
+    cidade.value = dados.localidade
+
+    let estado = document.getElementById('estado')
+    estado.value = dados.uf
+}
+
+const getDadosCepAPI = async function (numeroCep){
+
+    let url = `https://viacep.com.br/ws/${numeroCep}/json/`
+    const response = await fetch(url)
+    const dadosCep = await response.json()
+
+    setDadosForm(dadosCep)
+}
+
+const getCepForm = function (){
+
+    let cep = document.getElementById('input-cep')
+
+    if(cep.value == '')
+        alert('Não é possível consultar o CEP em branco!')
+    else
+        getDadosCepAPI(cep.value)
+}
+
+const cleanCepForm = function (){
+
+    var formulario = document.getElementById("formulario")
+
+    var elementos = formulario.elements;
+
+    for (var i = 0; i < elementos.length; i++) {
+        if (elementos[i].type === "text" || elementos[i].type === "textarea") {
+            elementos[i].value = "";
+        }
+    }
+}
+
+botaoSalvar.addEventListener('click', function(){
+    getCepForm()
+})
+
+botaoLimpar.addEventListener('click', function(){
+    cleanCepForm()
+})
